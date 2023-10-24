52668d7bc44cbf09b66c26795242a928c9b77740
Haqqi <haqqisetiadjie@student.uns.ac.id>
2023-10-24 23:29:58 +0700

    add_mul_div

--- Main.py
+++ Main.py
@@ -1,6 +1,5 @@
 from math_function import add
 
-
 def main():
 
     data_1 = int(input("masukkan input 1 :"))
@@ -9,6 +8,17 @@ def main():
 
     if operator == "+":
         result = add(data_1, data_2)
+    elif operator == "*":  # Check for multiplication
+        result = multiply(data_1, data_2)
+    elif operator == "/":  # Check for division
+        try:
+            result = divide(data_1, data_2)
+        except ValueError as e:
+            print("Error:", e)
+            return  # Exit the program in case of division by zero
+    else:
+        print("Operator not supported")
+        return  # Exit the program for unsupported operators
 
     print("{} {} {} = {} ".format(data_1, operator, data_2, result))
 
